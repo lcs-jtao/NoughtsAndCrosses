423544bfd860009b75e0ab1b2be61e09c2cd5800
Joyce Tao <jtao@lcs.on.ca>
2022-05-02 15:01:53 -0400

    Starting to organize the project

---
+++ NoughtsAndCrosses/Views/GameBoardView.swift
@@ -0,0 +1,20 @@
+//
+//  GameBoardView.swift
+//  NoughtsAndCrosses
+//
+//  Created by Joyce Tao on 2022-05-02.
+//
+
+import SwiftUI
+
+struct GameBoardView: View {
+    var body: some View {
+        Text("This is the game board")
+    }
+}
+
+struct GameBoardView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameBoardView()
+    }
+}
